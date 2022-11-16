3137c939fbea2ce30029078b01a31478792b9b6d
Susu-spec <suleimansuwaibat@gmail.com>
2022-11-16 10:24:15 -0800

    Add: print prompt

---
+++ prompt.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * prompt - checks mode and prints promt if interactive
+ * @fd: file descriptor/stream
+ * @buf: buffer
+ */
+
+void prompt(int fd, struct stat buf)
+{
+	fstat(fd, &buf);
+
+	if (S_ISCHR(buf.st_mode))
+		_puts(PROMPT);
+}
+
+/**
+ * _puts - writes to stdout
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	unsigned iint length;
+
+	length = _strlen(str);
+
+	write(STDOUT_FILENO, str, length);
+}
